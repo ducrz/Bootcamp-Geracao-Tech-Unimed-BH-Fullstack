3874952e09f3b4d277f8305cba5c3f23cf053432
Eduardo Monteiro <ducatolico@gmail.com>
2022-06-08 11:18:41 -0300

    Fim Curso: Primeiros passos com .NET

---
+++ Dominando-o-Ecossistema-.NET-com-C#/Primeiros-Passos-com-.NET/DigitalInnovationOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroVezes = 10;
+
+            for (int i = 0; i < numeroVezes; i++)
+            {
+                Console.WriteLine($"Bem-vindo ao curso de .NET! {i}");
+            }
+        }
+    }
+}
